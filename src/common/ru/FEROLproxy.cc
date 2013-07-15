c641870a11453b20dd994ad6f675ba8c90f96bf3
Remi Mommsen <remigius.mommsen@cern.ch>
2013-07-15 17:58:42 +0200

    Handle super-fragment.

--- src/common/ru/FEROLproxy.cc
+++ src/common/ru/FEROLproxy.cc
@@ -3,17 +3,23 @@
 
 bool evb::ru::RUinput::FEROLproxy::getSuperFragmentWithEvBid(const EvBid& evbId, readoutunit::FragmentChainPtr& superFragment)
 {
-  // boost::upgrade_lock<boost::shared_mutex> upgradeLock(superFragmentMapMutex_);
+  {
+    boost::mutex::scoped_lock sl(superFragmentFIFOmutex_);
+    if ( ! superFragmentFIFO_.deq(superFragment) ) return false;
+  }
   
-  // SuperFragmentMap::iterator fragmentPos = superFragmentMap_.find(evbId);
+  if ( superFragment->getEvBid() != evbId )
+  {
+    std::stringstream oss;
+    
+    oss << "Mismatch detected: expected evb id "
+      << evbId << ", but found evb id "
+      << superFragment->getEvBid() << " in data block.";
+    
+    XCEPT_RAISE(exception::MismatchDetected, oss.str());
+  }
   
-  // if ( fragmentPos == superFragmentMap_.end() || !fragmentPos->second->isComplete() ) return false;
-  
-  // superFragment = fragmentPos->second;
-  // superFragmentMap_.erase(fragmentPos);
-  // return true;
-
-  return false;
+  return true;
 }
 
 
