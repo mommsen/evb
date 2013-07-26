2544dd0e426673102660cf436fc92fab372ce5ed
Remi Mommsen <remigius.mommsen@cern.ch>
2013-07-26 12:49:24 +0200

    Increment version.

--- include/evb/version.h
+++ include/evb/version.h
@@ -4,8 +4,8 @@
 #include "config/PackageInfo.h"
 
 #define EVB_VERSION_MAJOR 1
-#define EVB_VERSION_MINOR 3
-#define EVB_VERSION_PATCH 1
+#define EVB_VERSION_MINOR 4
+#define EVB_VERSION_PATCH 0
 #undef EVB_PREVIOUS_VERSIONS
 
 
