da90f6cefa61dfa26d16def0cf5465ee78688464
Remi Mommsen <remigius.mommsen@cern.ch>
2013-08-02 12:41:45 +0200

    Expand disk-writing test case.

--- test/cases/2x1_write/controlScript.template.sh
+++ test/cases/2x1_write/controlScript.template.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Cleanup
-rm -rf /tmp/fff
+testDir=/tmp/rubuilder_test
+rm -rf $testDir
 
 # Launch executive processes
 sendCmdToLauncher EVM0_SOAP_HOST_NAME EVM0_LAUNCHER_PORT STARTXDAQEVM0_SOAP_PORT
@@ -39,19 +40,27 @@ sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT pt::atcp::PeerTransportATCP 
 sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT pt::atcp::PeerTransportATCP 2 Enable
 
 # Configure all applications
+setParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 rawDataDir string $testDir
+setParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 metaDataDir string $testDir
+
 sendSimpleCmdToApp EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 Configure
 sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 Configure
 sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 Configure
 
 sleep 1
 
+runNumber=`date "+%s"`
+
 #Enable EVM
+setParam EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 runNumber unsignedInt $runNumber
 sendSimpleCmdToApp EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 Enable
 
 #Enable RU
+setParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 runNumber unsignedInt $runNumber
 sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 Enable
 
 #Enable BUs
+setParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 runNumber unsignedInt $runNumber
 sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 Enable
 
 echo "Sending data for 5 seconds"
@@ -102,13 +111,81 @@ sendSimpleCmdToApp EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 Stop
 
 sleep 2
 
+state=`getParam EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 stateName xsd:string`
+echo "EVM state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
 #Stop RU
 sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 Stop
+
+state=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 stateName xsd:string`
+echo "RU state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
 
 #Stop BUs
 sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 Stop
 
+state=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 stateName xsd:string`
+echo "BU state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
 
+ls -l $testDir/BU-000/run$runNumber
+
+if [[ -x $testDir/BU-000/run$runNumber/open ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/open exists"
+    exit 1
+fi
+
+if [[ ! -s $testDir/BU-000/run$runNumber/rawData.jsd ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/rawData.jsd does not exist"
+    exit 1
+fi
+
+rawFiles=`ls $testDir/BU-000/run$runNumber/ls*raw | wc -l`
+jsnFiles=`ls $testDir/BU-000/run$runNumber/ls*jsn | wc -l`
+if [[ $rawFiles != $jsnFiles ]]
+then
+    echo "Test failed: found $rawFiles raw files, but $jsnFiles JSON files."
+    exit 1
+fi
+
+if [[ ! -s $testDir/BU-000/run$runNumber/EoLS.jsd ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/EoLS.jsd does not exist"
+    exit 1
+fi
+
+if [[ ! -s $testDir/BU-000/run$runNumber/EoLS_0000.jsn ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/EoLS_0000.jsn does not exist"
+    exit 1
+fi
+
+if [[ ! -s $testDir/BU-000/run$runNumber/EoR.jsd ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/EoR.jsd does not exist"
+    exit 1
+fi
+
+if [[ ! -s $testDir/BU-000/run$runNumber/EoR_$runNumber.jsn ]]
+then
+    echo "Test failed: $testDir/BU-000/run$runNumber/EoR_$runNumber.jsn does not exist"
+    exit 1
+fi
 
 echo "Test completed successfully"
 exit 0
