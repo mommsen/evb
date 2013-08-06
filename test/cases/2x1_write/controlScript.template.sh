7c04bfb8980715c2f1244308750f6fb773f66a0c
Remi Mommsen <remigius.mommsen@cern.ch>
2013-08-06 17:30:24 +0200

    Expanded checks on JSON files.

--- test/cases/2x1_write/controlScript.template.sh
+++ test/cases/2x1_write/controlScript.template.sh
@@ -82,14 +82,6 @@ then
   exit 1
 fi
 
-eventRateEVM=`getParam EVM0_SOAP_HOST_NAME EVM0_SOAP_PORT evb::EVM 0 eventRate xsd:unsignedInt`
-echo "EVM eventRate: $eventRateEVM"
-if [[ $eventRateEVM -lt 1000 ]]
-then
-  echo "Test failed"
-  exit 1
-fi
-
 nbEventsBuiltBU0=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 nbEventsBuilt xsd:unsignedInt`
 echo "BU0 nbEventsBuilt: $nbEventsBuiltBU0"
 if [[ $nbEventsBuiltBU0 -lt 1000 ]]
@@ -118,6 +110,9 @@ then
   echo "Test failed"
   exit 1
 fi
+
+echo "Wait for lumi section timeouts"
+sleep 15
 
 #Stop RU
 sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::RU 0 Stop
@@ -155,23 +150,9 @@ then
     exit 1
 fi
 
-rawFiles=`ls $testDir/BU-000/run$runNumber/ls*raw | wc -l`
-jsnFiles=`ls $testDir/BU-000/run$runNumber/ls*jsn | wc -l`
-if [[ $rawFiles != $jsnFiles ]]
-then
-    echo "Test failed: found $rawFiles raw files, but $jsnFiles JSON files."
-    exit 1
-fi
-
 if [[ ! -s $testDir/BU-000/run$runNumber/EoLS.jsd ]]
 then
     echo "Test failed: $testDir/BU-000/run$runNumber/EoLS.jsd does not exist"
-    exit 1
-fi
-
-if [[ ! -s $testDir/BU-000/run$runNumber/EoLS_0001.jsn ]]
-then
-    echo "Test failed: $testDir/BU-000/run$runNumber/EoLS_0000.jsn does not exist"
     exit 1
 fi
 
@@ -181,9 +162,92 @@ then
     exit 1
 fi
 
-if [[ ! -s $testDir/BU-000/run$runNumber/EoR_$runNumber.jsn ]]
+runEventCounter=0;
+runFileCounter=0;
+runLsCounter=0;
+
+for eolsFile in `ls $testDir/BU-000/run$runNumber/EoLS_*jsn`
+do
+    lumiSection=$(echo $eolsFile | sed -re 's/.*EoLS_([0-9]+).jsn/\1/')
+    lsEventCount=$(sed -nre 's/   "data" : \[ "([0-9]+)", "[0-9]+" \],/\1/p' $eolsFile)
+    lsFileCount=$(sed -nre 's/   "data" : \[ "[0-9]+", "([0-9]+)" \],/\1/p' $eolsFile)
+
+    eventCounter=0
+    fileCounter=0
+
+    if [[ $lsFileCount -gt 0 ]]
+    then
+        ((runLsCounter++))
+
+        for file in `ls $testDir/BU-000/run$runNumber/run${runNumber}_ls${lumiSection}_*raw`
+        do
+            ((fileCounter++))
+            ((runFileCounter++))
+            fileSize=$(stat -c%s $file)
+            expectedEventCount=$(($fileSize/24576))
+
+            jsonFile=$(echo $file | sed -re 's/.raw$/.jsn/')
+            if [[ ! -s $jsonFile ]]
+            then
+                echo "Test failed: $jsonFile does not exist"
+                exit 1
+            fi
+
+            eventCount=$(sed -nre 's/   "data" : \[ "([0-9]+)" \],/\1/p' $jsonFile)
+            if [[ $eventCount != $expectedEventCount ]]
+            then
+                echo "Test failed: expected $expectedEventCount, but found $eventCount events in JSON file"
+                exit 1
+            fi
+            eventCounter=$(($eventCounter+$eventCount))
+        done
+
+    else
+
+        fileCounter=$(ls $testDir/BU-000/run$runNumber/*[raw,jsn] | grep -c run${runNumber}_ls${lumiSection})
+
+    fi
+
+    if [[ $eventCounter != $lsEventCount ]]
+    then
+        echo "Test failed: expected $lsEventCount events in LS $lumiSection, but found $eventCounter events in raw data JSON files"
+        exit 1
+    fi
+    if [[ $fileCounter != $lsFileCount ]]
+    then
+        echo "Test failed: expected $lsFileCount files for LS $lumiSection, but found $fileCounter raw data files"
+        exit 1
+    fi
+
+    runEventCounter=$(($runEventCounter+$eventCounter))
+
+done
+
+eorFile="$testDir/BU-000/run$runNumber/EoR_$runNumber.jsn"
+
+if [[ ! -s $eorFile ]]
 then
-    echo "Test failed: $testDir/BU-000/run$runNumber/EoR_$runNumber.jsn does not exist"
+    echo "Test failed: cannot locate the EoR json file $eorFile"
+    exit 1
+fi
+
+runEventCount=$(sed -nre 's/   "data" : \[ "([0-9]+)", "[0-9]+", "[0-9]+" \],/\1/p' $eorFile)
+runFileCount=$(sed -nre 's/   "data" : \[ "[0-9]+", "([0-9]+)", "[0-9]+" \],/\1/p' $eorFile)
+runLsCount=$(sed -nre 's/   "data" : \[ "[0-9]+", "[0-9]+", "([0-9]+)" \],/\1/p' $eorFile)
+
+if [[ $runEventCounter != $runEventCount ]]
+then
+    echo "Test failed: expected $runEventCount events in run $runNumber, but found $runEventCounter events in raw data JSON files"
+    exit 1
+fi
+if [[ $runFileCounter != $runFileCount ]]
+then
+    echo "Test failed: expected $runFileCount files for run $runNumber, but found $runFileCounter raw data files"
+    exit 1
+fi
+if [[ $runLsCounter != $runLsCount ]]
+then
+    echo "Test failed: expected $runLsCount lumi sections for run $runNumber, but found raw data files from $runLsCounter lumi sections"
     exit 1
 fi
 
