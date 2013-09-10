87aa63e146e4c7f6ff732fbccc96cafa8c277dda
Remi Mommsen <remigius.mommsen@cern.ch>
2013-09-10 17:04:37 +0200

    Actually use the precompiler switch.

--- src/common/CRCCalculator.cc
+++ src/common/CRCCalculator.cc
@@ -167,6 +167,8 @@ uint16_t evb::CRCCalculator::compute(const uint8_t* buffer, size_t bufSize) cons
 
 void evb::CRCCalculator::compute(uint16_t& crc, const uint8_t* buffer, size_t bufSize) const
 {
+  #ifdef EVB_CALCULATE_CRC
+
   assert(0==bufSize%8);
   if ( bufSize == 0 ) return;
 
@@ -200,6 +202,8 @@ void evb::CRCCalculator::compute(uint16_t& crc, const uint8_t* buffer, size_t bu
       computeCRC_32bit(crc, ((uint32_t *)buffer)[0]);
     }
   }
+
+  #endif //EVB_CALCULATE_CRC
 }
 
 
