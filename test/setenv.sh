bbb13120f287db32ae996de4951437d1bcbc1335
Remi Mommsen <remigius.mommsen@cern.ch>
2013-10-02 15:50:51 +0200

    Need to set XDAQ_PLATFORM for tests.

--- test/setenv.sh
+++ test/setenv.sh
@@ -2,6 +2,13 @@
 export XDAQ_ROOT=/opt/xdaq
 #export XDAQ_ROOT=$HOME/daq/xdaq_root
 
+export XDAQ_PLATFORM=`uname -m`
+if test ".$XDAQ_PLATFORM" != ".x86_64"; then
+    export XDAQ_PLATFORM=x86
+fi
+checkos=`$XDAQ_ROOT/config/checkos.sh`
+export XDAQ_PLATFORM=$XDAQ_PLATFORM"_"$checkos
+
 export XDAQ_LOCAL=${XDAQ_ROOT}
 #export XDAQ_LOCAL=${HOME}/daq/trunk/${XDAQ_PLATFORM}
 export XDAQ_DOCUMENT_ROOT=${XDAQ_ROOT}/htdocs
