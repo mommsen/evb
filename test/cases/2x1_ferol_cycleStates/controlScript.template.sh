468f11725f4de2d81f26367e575cc70f5564b3e5
Remi Mommsen <remigius.mommsen@cern.ch>
2014-03-21 14:22:41 +0100

    Test case for state changes with FEROL input.

---
+++ test/cases/2x1_ferol_cycleStates/controlScript.template.sh
@@ -0,0 +1,359 @@
+#!/bin/sh
+
+function sendCmdToPt
+{
+  sendSimpleCmdToApp FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT pt::frl::Application 0 $1
+  sendSimpleCmdToApp FEROL1_SOAP_HOST_NAME FEROL1_SOAP_PORT pt::frl::Application 1 $1
+  sendSimpleCmdToApp FEROL2_SOAP_HOST_NAME FEROL2_SOAP_PORT pt::frl::Application 2 $1
+  sendSimpleCmdToApp FEROL3_SOAP_HOST_NAME FEROL3_SOAP_PORT pt::frl::Application 3 $1
+  sendSimpleCmdToApp FEROL4_SOAP_HOST_NAME FEROL4_SOAP_PORT pt::frl::Application 4 $1
+  sendSimpleCmdToApp FEROL5_SOAP_HOST_NAME FEROL5_SOAP_PORT pt::frl::Application 5 $1
+  sendSimpleCmdToApp FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT pt::frl::Application 6 $1
+  sendSimpleCmdToApp FEROL7_SOAP_HOST_NAME FEROL7_SOAP_PORT pt::frl::Application 7 $1
+
+  sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT pt::utcp::Application 0 $1
+  sendSimpleCmdToApp RU1_SOAP_HOST_NAME RU1_SOAP_PORT pt::utcp::Application 0 $1
+  sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT pt::utcp::Application 1 $1
+}
+
+function sendCmdToFEROL
+{
+  sendSimpleCmdToApp FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT evb::test::DummyFEROL 0 $1
+  sendSimpleCmdToApp FEROL1_SOAP_HOST_NAME FEROL1_SOAP_PORT evb::test::DummyFEROL 1 $1
+  sendSimpleCmdToApp FEROL2_SOAP_HOST_NAME FEROL2_SOAP_PORT evb::test::DummyFEROL 2 $1
+  sendSimpleCmdToApp FEROL3_SOAP_HOST_NAME FEROL3_SOAP_PORT evb::test::DummyFEROL 3 $1
+  sendSimpleCmdToApp FEROL4_SOAP_HOST_NAME FEROL4_SOAP_PORT evb::test::DummyFEROL 4 $1
+  sendSimpleCmdToApp FEROL5_SOAP_HOST_NAME FEROL5_SOAP_PORT evb::test::DummyFEROL 5 $1
+  sendSimpleCmdToApp FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT evb::test::DummyFEROL 6 $1
+  sendSimpleCmdToApp FEROL7_SOAP_HOST_NAME FEROL7_SOAP_PORT evb::test::DummyFEROL 7 $1
+}
+
+function sendCmdToEvB
+{
+  sendSimpleCmdToApp RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 $1
+  sendSimpleCmdToApp RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 $1
+  sleep 1
+  sendSimpleCmdToApp BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 $1
+}
+
+function changeStates
+{
+  if [[ $1 == 'Stop' ]]; then
+    sendCmdToFEROL $1
+    sleep 5
+    sendCmdToEvB $1
+  else
+    sendCmdToEvB $1
+    sendCmdToFEROL $1
+  fi
+}
+
+function run
+{
+  # Configure all applications
+  changeStates Configure
+  sleep 1
+
+  #Enable the system
+  setParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 runNumber unsignedInt $1
+  setParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 runNumber unsignedInt $1
+  setParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 runNumber unsignedInt $1
+  changeStates Enable
+
+  echo "Sending data for 5 seconds"
+  sleep 5
+
+  superFragmentSizeEVM=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 superFragmentSize xsd:unsignedInt`
+  echo "EVM superFragmentSize: $superFragmentSizeEVM"
+  if [[ $superFragmentSizeEVM -ne 8192 ]]
+  then
+    echo "Test failed: expected 8192"
+    exit 1
+  fi
+
+  superFragmentSizeRU1=`getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 superFragmentSize xsd:unsignedInt`
+  echo "RU1 superFragmentSize: $superFragmentSizeRU1"
+  if [[ $superFragmentSizeRU1 -ne 8192 ]]
+  then
+    echo "Test failed: expected 8192"
+    exit 1
+  fi
+
+  eventRateEVM=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 eventRate xsd:unsignedInt`
+  echo "EVM eventRate: $eventRateEVM"
+  if [[ $eventRateEVM -lt 500 ]]
+  then
+    echo "Test failed"
+    exit 1
+  fi
+
+  eventCountEVM=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 eventCount xsd:unsignedInt`
+  echo "EVM eventCount: $eventCountEVM"
+  if [[ $eventCountEVM -lt 500 ]]
+  then
+    echo "Test failed"
+    exit 1
+  fi
+
+  nbEventsBuiltBU0=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 nbEventsBuilt xsd:unsignedInt`
+  echo "BU0 nbEventsBuilt: $nbEventsBuiltBU0"
+  if [[ $nbEventsBuiltBU0 -lt 1000 ]]
+  then
+    echo "Test failed"
+    exit 1
+  fi
+
+  eventSizeBU0=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 eventSize xsd:unsignedInt`
+  echo "BU0 eventSize: $eventSizeBU0"
+  if [[ $eventSizeBU0 -ne 16384 ]]
+  then
+    echo "Test failed: expected 16384"
+    exit 1
+  fi
+}
+
+# Launch executive processes
+sendCmdToLauncher FEROL0_SOAP_HOST_NAME FEROL0_LAUNCHER_PORT STARTXDAQFEROL0_SOAP_PORT
+sendCmdToLauncher FEROL1_SOAP_HOST_NAME FEROL1_LAUNCHER_PORT STARTXDAQFEROL1_SOAP_PORT
+sendCmdToLauncher FEROL2_SOAP_HOST_NAME FEROL2_LAUNCHER_PORT STARTXDAQFEROL2_SOAP_PORT
+sendCmdToLauncher FEROL3_SOAP_HOST_NAME FEROL3_LAUNCHER_PORT STARTXDAQFEROL3_SOAP_PORT
+sendCmdToLauncher FEROL4_SOAP_HOST_NAME FEROL4_LAUNCHER_PORT STARTXDAQFEROL4_SOAP_PORT
+sendCmdToLauncher FEROL5_SOAP_HOST_NAME FEROL5_LAUNCHER_PORT STARTXDAQFEROL5_SOAP_PORT
+sendCmdToLauncher FEROL6_SOAP_HOST_NAME FEROL6_LAUNCHER_PORT STARTXDAQFEROL6_SOAP_PORT
+sendCmdToLauncher FEROL7_SOAP_HOST_NAME FEROL7_LAUNCHER_PORT STARTXDAQFEROL7_SOAP_PORT
+sendCmdToLauncher RU0_SOAP_HOST_NAME RU0_LAUNCHER_PORT STARTXDAQRU0_SOAP_PORT
+sendCmdToLauncher RU1_SOAP_HOST_NAME RU1_LAUNCHER_PORT STARTXDAQRU1_SOAP_PORT
+sendCmdToLauncher BU0_SOAP_HOST_NAME BU0_LAUNCHER_PORT STARTXDAQBU0_SOAP_PORT
+
+# Check that executives are listening
+if ! webPingXDAQ FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL1_SOAP_HOST_NAME FEROL1_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL2_SOAP_HOST_NAME FEROL2_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL3_SOAP_HOST_NAME FEROL3_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL4_SOAP_HOST_NAME FEROL4_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL5_SOAP_HOST_NAME FEROL5_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ FEROL7_SOAP_HOST_NAME FEROL7_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ RU0_SOAP_HOST_NAME RU0_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ RU1_SOAP_HOST_NAME RU1_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+if ! webPingXDAQ BU0_SOAP_HOST_NAME BU0_SOAP_PORT 5
+then
+  echo "Test failed"
+  exit 1
+fi
+
+# Configure all executives
+sendCmdToExecutive FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL1_SOAP_HOST_NAME FEROL1_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL2_SOAP_HOST_NAME FEROL2_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL3_SOAP_HOST_NAME FEROL3_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL4_SOAP_HOST_NAME FEROL4_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL5_SOAP_HOST_NAME FEROL5_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive FEROL7_SOAP_HOST_NAME FEROL7_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive RU0_SOAP_HOST_NAME RU0_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive RU1_SOAP_HOST_NAME RU1_SOAP_PORT configure.cmd.xml
+sendCmdToExecutive BU0_SOAP_HOST_NAME BU0_SOAP_PORT configure.cmd.xml
+
+# Stop generating events in an orderly manner
+eventNumber=44444
+setParam FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT evb::test::DummyFEROL 0 stopAtEvent unsignedInt $eventNumber
+setParam FEROL1_SOAP_HOST_NAME FEROL1_SOAP_PORT evb::test::DummyFEROL 1 stopAtEvent unsignedInt $eventNumber
+setParam FEROL2_SOAP_HOST_NAME FEROL2_SOAP_PORT evb::test::DummyFEROL 2 stopAtEvent unsignedInt $eventNumber
+setParam FEROL3_SOAP_HOST_NAME FEROL3_SOAP_PORT evb::test::DummyFEROL 3 stopAtEvent unsignedInt $eventNumber
+setParam FEROL4_SOAP_HOST_NAME FEROL4_SOAP_PORT evb::test::DummyFEROL 4 stopAtEvent unsignedInt $eventNumber
+setParam FEROL5_SOAP_HOST_NAME FEROL5_SOAP_PORT evb::test::DummyFEROL 5 stopAtEvent unsignedInt $eventNumber
+setParam FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT evb::test::DummyFEROL 6 stopAtEvent unsignedInt $eventNumber
+setParam FEROL7_SOAP_HOST_NAME FEROL7_SOAP_PORT evb::test::DummyFEROL 7 stopAtEvent unsignedInt $eventNumber
+
+#Enable pt
+sendCmdToPt Configure
+sendCmdToPt Enable
+
+echo "Starting run 1"
+run 1
+
+echo "Halt the system"
+changeStates Halt
+
+state=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 stateName xsd:string`
+echo "EVM state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 stateName xsd:string`
+echo "RU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 stateName xsd:string`
+echo "BU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+echo "Restart run 2"
+run 2
+
+while [[ `getParam FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT evb::test::DummyFEROL 0 lastEventNumber xsd:unsignedInt` > \
+         `getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 lastEventNumber xsd:unsignedInt` ]]
+do
+  sleep 1
+done
+
+changeStates Stop
+sleep 2
+
+state=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 stateName xsd:string`
+echo "EVM state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 stateName xsd:string`
+echo "RU state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 stateName xsd:string`
+echo "BU state=$state"
+if [[ "$state" != "Ready" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+eventCountEVM=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 eventCount xsd:unsignedInt`
+nbEventsBuiltBU0=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 nbEventsBuilt xsd:unsignedInt`
+if [[ $eventCountEVM != $nbEventsBuiltBU0 ]]
+then
+  echo "Event counts do not match: EVM $eventCountEVM vs BU $nbEventsBuiltBU0"
+  exit 1
+fi
+
+echo "Halt the system"
+changeStates Halt
+
+state=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 stateName xsd:string`
+echo "EVM state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 stateName xsd:string`
+echo "RU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 stateName xsd:string`
+echo "BU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+echo "Restart run 3"
+run 3
+
+while [[ `getParam FEROL0_SOAP_HOST_NAME FEROL0_SOAP_PORT evb::test::DummyFEROL 0 lastEventNumber xsd:unsignedInt` > \
+         `getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 lastEventNumber xsd:unsignedInt` ]]
+do
+  sleep 1
+done
+
+echo "Halt the system"
+changeStates Halt
+
+state=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 stateName xsd:string`
+echo "EVM state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 stateName xsd:string`
+echo "RU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+state=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 stateName xsd:string`
+echo "BU state=$state"
+if [[ "$state" != "Halted" ]]
+then
+  echo "Test failed"
+  exit 1
+fi
+
+eventCountEVM=`getParam RU0_SOAP_HOST_NAME RU0_SOAP_PORT evb::EVM 0 eventCount xsd:unsignedInt`
+nbEventsBuiltBU0=`getParam BU0_SOAP_HOST_NAME BU0_SOAP_PORT evb::BU 0 nbEventsBuilt xsd:unsignedInt`
+if [[ $eventCountEVM != $nbEventsBuiltBU0 ]]
+then
+  echo "Event counts do not match: EVM $eventCountEVM vs BU $nbEventsBuiltBU0"
+  exit 1
+fi
+
+echo "Test completed successfully"
+exit 0
