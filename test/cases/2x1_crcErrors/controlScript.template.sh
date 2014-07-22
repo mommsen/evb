325e90aeee107e6b9bdd47548432f940e4fbfdad
Remi Mommsen <remigius.mommsen@cern.ch>
2014-07-22 15:48:07 +0200

    Increase sleep time after CRC error storm.

--- test/cases/2x1_crcErrors/controlScript.template.sh
+++ test/cases/2x1_crcErrors/controlScript.template.sh
@@ -256,7 +256,7 @@ fi
 echo "100 CRC errors on FED 6"
 setParam FEROL6_SOAP_HOST_NAME FEROL6_SOAP_PORT evb::test::DummyFEROL 6 nbCRCerrors unsignedInt 100
 
-sleep 3
+sleep 5
 
 state=$(getParam RU1_SOAP_HOST_NAME RU1_SOAP_PORT evb::RU 0 stateName xsd:string)
 echo "RU state=$state"
