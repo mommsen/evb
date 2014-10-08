d9dcd26d7912941ac376deceadff8bcd376becce
Remi Mommsen <remigius.mommsen@cern.ch>
2014-10-08 11:51:22 +0200

    Migrate FEROL test cases

---
+++ test/cases/1x2_ferol.py
@@ -0,0 +1,31 @@
+from TestCase import TestCase
+from Configuration import FEROL,RU,BU
+
+
+class case_1x2_ferol(TestCase):
+
+    def runTest(self):
+        self.configureEvB()
+        self.enableEvB()
+        self.checkEVM(16384)
+        self.checkBU(16384)
+        self.haltEvB()
+
+
+    def fillConfiguration(self,symbolMap):
+        fedIds = range(1,9)
+        evm = RU(symbolMap,[
+             ('inputSource','string','FEROL'),
+             ('fedSourceIds','unsignedInt',fedIds)
+            ])
+        for id in fedIds:
+            self._config.add( FEROL(symbolMap,evm,[
+                ('fedId','unsignedInt',str(id))
+                ]) )
+        self._config.add( evm )
+        self._config.add( BU(symbolMap,[
+             ('dropEventData','boolean','true')
+            ]) )
+        self._config.add( BU(symbolMap,[
+             ('dropEventData','boolean','true')
+            ]) )
