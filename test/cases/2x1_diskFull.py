06180d586e3f12d76c23f609d2d117dd50474747
Remi Mommsen <remigius.mommsen@cern.ch>
2015-04-01 10:07:26 +0200

    Adjust timing for faster BU

--- test/cases/2x1_diskFull.py
+++ test/cases/2x1_diskFull.py
@@ -17,7 +17,7 @@ class case_2x1_diskFull(TestCase):
         runDir=testDir+"/run"+runNumber
         diskUsage = self.prepareAppliance(testDir,runNumber)
         self.setAppParam('rawDataLowWaterMark','double',str(diskUsage),'BU')
-        self.setAppParam('rawDataHighWaterMark','double',str(diskUsage+0.001),'BU')
+        self.setAppParam('rawDataHighWaterMark','double',str(diskUsage+0.05),'BU')
         self.setAppParam('rawDataDir','string',testDir,'BU')
         self.setAppParam('metaDataDir','string',testDir,'BU')
         self.configureEvB()
@@ -25,11 +25,9 @@ class case_2x1_diskFull(TestCase):
         self.enableEvB(runNumber=runNumber,sleepTime=0)
         sys.stdout.write("Running until disk is full...")
         sys.stdout.flush()
-        time.sleep(10)
-        self.checkAppState("Throttled","BU")
         self.waitForAppParam('eventRate','unsignedInt',0,operator.eq,'EVM')
         # avoid some spurious wake ups
-        time.sleep(1)
+        time.sleep(3)
         self.waitForAppParam('eventRate','unsignedInt',0,operator.eq,'EVM')
         print(" done")
         self.checkAppState("Enabled","EVM")
