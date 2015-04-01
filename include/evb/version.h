707a1f776c580cc73e0d4534cfbfeb8031178823
Remi Mommsen <remigius.mommsen@cern.ch>
2015-04-01 10:19:41 +0200

    Increment version to 2.11.0

--- include/evb/version.h
+++ include/evb/version.h
@@ -4,8 +4,8 @@
 #include "config/PackageInfo.h"
 
 #define EVB_VERSION_MAJOR 2
-#define EVB_VERSION_MINOR 10
-#define EVB_VERSION_PATCH 1
+#define EVB_VERSION_MINOR 11
+#define EVB_VERSION_PATCH 0
 #undef EVB_PREVIOUS_VERSIONS
 
 
