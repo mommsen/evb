5e1e3437347fd9e9260716ec6e6c60e7c2d93cf5
Remi Mommsen <remigius.mommsen@cern.ch>
2015-07-24 14:53:04 +0200

    Add test case with virgin-raw like FED sizes

---
+++ test/cases/2x1_hugeEvents.py
@@ -0,0 +1,40 @@
+import operator
+
+from TestCase import TestCase
+from Configuration import FEROL,RU,BU
+
+
+class case_2x1_hugeEvents(TestCase):
+
+    def runTest(self):
+        self.configureEvB()
+        self.enableEvB()
+        self.checkAppParam("eventRate","unsignedInt",1000,operator.gt,"EVM")
+        self.checkAppParam("nbEventsBuilt","unsignedLong",1000,operator.gt,"BU")
+        self.haltEvB()
+
+
+    def fillConfiguration(self,symbolMap):
+        evm = RU(symbolMap,[
+             ('inputSource','string','Socket')
+            ])
+        for id in range(0,4):
+            self._config.add( FEROL(symbolMap,evm,id,[
+                ('fedSize','unsignedInt','49624')
+                ]) )
+
+        ru = RU(symbolMap,[
+             ('inputSource','string','Socket')
+            ])
+        for id in range(4,8):
+            self._config.add( FEROL(symbolMap,ru,id,[
+                ('fedSize','unsignedInt','49624')
+                ]) )
+
+        self._config.add( evm )
+        self._config.add( ru )
+
+        self._config.add( BU(symbolMap,[
+             ('dropEventData','boolean','true'),
+             ('lumiSectionTimeout','unsignedInt','0')
+            ]) )
