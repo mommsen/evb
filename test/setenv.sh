7e8d5dbee4915ef5c1b312dea1acd9193e5b6617
Remi Mommsen <remigius.mommsen@cern.ch>
2016-02-09 10:00:34 +0100

    Update environments

--- test/setenv.sh
+++ test/setenv.sh
@@ -9,15 +9,13 @@ fi
 checkos=`$XDAQ_ROOT/config/checkos.sh`
 export XDAQ_PLATFORM=$XDAQ_PLATFORM"_"$checkos
 
-export XDAQ_LOCAL=${XDAQ_ROOT}
-#export XDAQ_LOCAL=${HOME}/daq/trunk/${XDAQ_PLATFORM}
+export XDAQ_EVB=${HOME}/daq/baseline13/${XDAQ_PLATFORM}
 export XDAQ_DOCUMENT_ROOT=${XDAQ_ROOT}/htdocs
-#export XDAQ_DOCUMENT_ROOT=${HOME}/daq/trunk/daq
-export LD_LIBRARY_PATH=${XDAQ_LOCAL}/lib:${XDAQ_ROOT}/lib:${LD_LIBRARY_PATH}
-export PATH=${PATH}:${XDAQ_LOCAL}/bin:${XDAQ_ROOT}/bin
+export LD_LIBRARY_PATH=${XDAQ_EVB}/lib:${XDAQ_ROOT}/lib:${LD_LIBRARY_PATH}
+export PATH=${PATH}:${XDAQ_EVB}/bin:${XDAQ_ROOT}/bin
 
 # EvB tester suite
-export EVB_TESTER_HOME=${HOME}/daq/trunk/daq/evb/test
+export EVB_TESTER_HOME=${HOME}/daq/baseline13/daq/evb/test
 export EVB_SYMBOL_MAP=standaloneSymbolMap.txt
 export PATH=${PATH}:${EVB_TESTER_HOME}/scripts
 export PYTHONPATH=${EVB_TESTER_HOME}/scripts:${PYTHONPATH}
