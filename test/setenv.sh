e4ed1772aa6861ad6e770ba4b44d85a56422d303
Remi Mommsen <remigius.mommsen@cern.ch>
2016-02-11 13:21:05 +0100

    Remove unused env vars

--- test/setenv.sh
+++ test/setenv.sh
@@ -1,6 +1,5 @@
 # XDAQ
 export XDAQ_ROOT=/opt/xdaq
-#export XDAQ_ROOT=$HOME/daq/xdaq_root
 
 export XDAQ_PLATFORM=`uname -m`
 if test ".$XDAQ_PLATFORM" != ".x86_64"; then
@@ -8,11 +7,7 @@ if test ".$XDAQ_PLATFORM" != ".x86_64"; then
 fi
 checkos=`$XDAQ_ROOT/config/checkos.sh`
 export XDAQ_PLATFORM=$XDAQ_PLATFORM"_"$checkos
-
 export XDAQ_EVB=${HOME}/daq/baseline13/${XDAQ_PLATFORM}
-export XDAQ_DOCUMENT_ROOT=${XDAQ_ROOT}/htdocs
-export LD_LIBRARY_PATH=${XDAQ_EVB}/lib:${XDAQ_ROOT}/lib:${LD_LIBRARY_PATH}
-export PATH=${PATH}:${XDAQ_EVB}/bin:${XDAQ_ROOT}/bin
 
 # EvB tester suite
 export EVB_TESTER_HOME=${HOME}/daq/baseline13/daq/evb/test
