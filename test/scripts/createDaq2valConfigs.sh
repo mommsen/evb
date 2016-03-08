8751a5a44a1edcab53fd910c7840a0288495e279
Remi Mommsen <remigius.mommsen@cern.ch>
2016-03-08 12:13:22 +0100

    Generate daq2val configurations using configurator

---
+++ test/scripts/createDaq2valConfigs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#swt=/DAQ2_Official/StandAlone/StandAlone_40g_infini_dropAtBU
+swt=/Test/Remi/StandAlone/StandAlone_40g_infini_dropAtBU
+dir=../daq2val
+mkdir -p $dir
+
+(
+cat <<'EOF'
+RUEVM_CHECK_CRC 0
+BU_CHECK_CRC 0
+BU_CALC_CRC32C false
+EOF
+) > /tmp/$$.txt
+
+for setting in '4s4f' '8s8f' '12s12f' '16s16f' '24s24f' '4s2f' '8s4f' '12s6f' '16s8f' '24s12f' ; do
+    ./createConfig.py /daq2val/eq_150914_emu/1fb_$setting $swt 2 $dir/${setting}x1x2_noChksums -s /tmp/$$.txt --daqval -p $HOME/configurator/CONFIGURATOR_DAQ2VAL.properties
+    ./createConfig.py /daq2val/eq_150914_emu/1fb_$setting $swt 2 $dir/${setting}x1x2 --daqval -p $HOME/configurator/CONFIGURATOR_DAQ2VAL.properties
+done
+
+rm -f /tmp/$$.txt
