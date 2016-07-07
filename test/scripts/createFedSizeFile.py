442ba53e2ea4cbabe9ac03a8c1e8956fa0912876
Remi Mommsen <remigius.mommsen@cern.ch>
2016-07-07 17:18:28 +0200

    Create FED size files as used by DAQ FM from parameter files

---
+++ test/scripts/createFedSizeFile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument("paramFile",help="calculate the FED fragment sizes using the parameters in the given file")
+parser.add_argument("relEventSize",type=float,help="scale the event size by this factor")
+parser.add_argument("outputFile",help="file name of the output file")
+args = vars(parser.parse_args())
+
+with open(args['outputFile'],'w') as outputFile:
+    outputFile.write("#fedId,size,rms\n")
+    with open(args['paramFile']) as paramFile:
+        for line in paramFile:
+            (fedIds,a,b,c,rms) = line.split(',',5)
+            try:
+                for id in fedIds.split('+'):
+                    fedSize = float(a)+float(b)*args['relEventSize']+float(c)*args['relEventSize']**2
+                    outputFile.write("%d,%d,%d\n" % (int(id),int(fedSize)&~0x7,int(fedSize*float(rms))&~0x7))
+            except ValueError:
+                pass
