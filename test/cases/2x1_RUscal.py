51f16277cbe755723bd4ace8496b6a7bdd5e279b
Remi Mommsen <remigius.mommsen@cern.ch>
2017-11-09 17:49:13 +0100

    Test interplay of softFED with FEROL inputs

--- test/cases/2x1_RUscal.py
+++ test/cases/2x1_RUscal.py
@@ -1,5 +1,5 @@
 from TestCase import TestCase
-from Context import RU,BU
+from Context import FEROL,RU,BU
 
 class case_2x1_RUscal(TestCase):
 
@@ -7,21 +7,28 @@ class case_2x1_RUscal(TestCase):
         self.configureEvB()
         self.enableEvB()
         self.checkEVM(6144)
-        self.checkRU(24696)
-        self.checkBU(30840)
+        self.checkRU(8312)
+        self.checkBU(14456)
         self.stopEvB()
         self.haltEvB()
 
 
     def fillConfiguration(self,symbolMap):
-        self._config.add( RU(symbolMap,[
-             ('inputSource','string','Local'),
-             ('fedSourceIds','unsignedInt',(512,25,17))
-            ]) )
-        self._config.add( RU(symbolMap,[
-             ('inputSource','string','Local'),
-             ('fedSourceIds','unsignedInt',range(1,13)+[1022,])
-            ]) )
+        evm = RU(symbolMap,[
+             ('inputSource','string','Socket')
+            ])
+        for id in (512,25,17):
+            self._config.add( FEROL(symbolMap,evm,id) )
+
+        ru = RU(symbolMap,[
+             ('inputSource','string','Socket')
+            ])
+        for id in range(1,5)+[1022,]:
+            self._config.add( FEROL(symbolMap,ru,id) )
+
+        self._config.add( evm )
+        self._config.add( ru )
+
         self._config.add( BU(symbolMap,[
              ('dropEventData','boolean','true'),
              ('lumiSectionTimeout','unsignedInt','0')
