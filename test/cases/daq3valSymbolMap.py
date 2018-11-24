575759d5a6e6c2809404e64579d67b6664b1a940
Remi Mommsen <remigius.mommsen@cern.ch>
2018-11-24 18:18:07 +0100

    Minor reshuffling of nodes

--- test/cases/daq3valSymbolMap.py
+++ test/cases/daq3valSymbolMap.py
@@ -6,7 +6,9 @@ data['FRL_BASE_PORT'] = 55320
 dataNetwork = ".d3vrbs1v0.cms"
 
 hosts = [
-    "d3vrubu-c2e34-06-01", # first RU
+#    "d3vrubu-c2e33-08-01", # EVM
+
+    "d3vrubu-c2e34-06-01",
     "d3vrubu-c2e34-08-01",
     "d3vrubu-c2e34-10-01",
     "d3vrubu-c2e34-12-01",
@@ -15,8 +17,7 @@ hosts = [
     "d3vrubu-c2e34-18-01",
     "d3vrubu-c2e34-20-01",
     "d3vrubu-c2e34-27-01",
-
-    "d3vrubu-c2e34-29-01", # first BU
+    "d3vrubu-c2e34-29-01",
     "d3vrubu-c2e34-31-01",
     "d3vrubu-c2e34-33-01",
     "d3vrubu-c2e34-35-01",
@@ -25,7 +26,6 @@ hosts = [
     "d3vrubu-c2e34-41-01",
 
     "d3vrubu-c2e33-06-01",
-    "d3vrubu-c2e33-08-01",
 ]
 
 # add RUs and BUs
@@ -35,6 +35,8 @@ buIndex = 0
 for host in hosts:
 
     if host in ("d3vrubu-c2e34-18-01"):
+#    if host in ("d3vrubu-c2e34-20-01"):
+#    if host in (""):
         data['BU%d_SOAP_HOST_NAME' % buIndex] = host + ".cms"
         data['BU%d_I2O_HOST_NAME' % buIndex] = host + dataNetwork
         buIndex += 1
